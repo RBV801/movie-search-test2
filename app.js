bebeef7266650df404c65c47e87b7c228b8cb9e8
RBV801 <rakmunbv@gmail.com>
2024-12-03 12:28:37 -0800

    Remove results limit, show total count

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ createApp({
         return {
             searchQuery: '',
             movies: [],
-            proxyUrl: 'http://localhost:3001' // Updated port to 3001
+            proxyUrl: 'http://localhost:3001'
         }
     },
     methods: {
@@ -18,6 +18,13 @@ createApp({
                 
                 if (data.Search) {
                     this.movies = data.Search
+                    
+                    while (data.totalResults > this.movies.length) {
+                        const page = Math.floor(this.movies.length / 10) + 1
+                        const nextPageResponse = await fetch(`${this.proxyUrl}/api/search?query=${encodeURIComponent(this.searchQuery)}&page=${page}`)
+                        const nextPageData = await nextPageResponse.json()
+                        this.movies.push(...nextPageData.Search)
+                    }
                 }
             } catch (error) {
                 console.error('Error fetching movies:', error)
