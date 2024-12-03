0ff3f06fcad5ae4ad386875d909d58cfcfd2b32c
RBV801 <rakmunbv@gmail.com>
2024-12-03 12:31:45 -0800

    Fetch additional data, filter adult, deduplicate

--- app.js
+++ app.js
@@ -7,10 +7,27 @@ createApp({
         return {
             searchQuery: '',
             movies: [],
-            proxyUrl: 'http://localhost:3001'
+            proxyUrl: 'http://localhost:3001',
+            recognition: null
+        }
+    },
+    mounted() {
+        this.recognition = new webkitSpeechRecognition()
+        this.recognition.continuous = false
+        this.recognition.lang = 'en-US'
+        this.recognition.interimResults = false
+        this.recognition.maxAlternatives = 1
+        
+        this.recognition.onresult = (event) => {
+            const transcript = event.results[0][0].transcript
+            this.searchQuery = transcript
+            this.searchMovies()
         }
     },
     methods: {
+        voiceSearch() {
+            this.recognition.start()
+        },
         async searchMovies() {
             if (!this.searchQuery) return
             
@@ -41,10 +58,41 @@ createApp({
                     }
                     
                     this.movies.sort((a, b) => b.popularity - a.popularity)
+                    
+                    await this.fetchAdditionalData()
+                    
+                    this.movies = this.deduplicate(this.movies)
+                    this.movies = this.movies.filter(movie => !movie.adult)
+                } else {
+                    this.movies = []
                 }
             } catch (error) {
                 console.error('Error fetching movies:', error)
             }
+        },
+        async fetchAdditionalData() {
+            for (const movie of this.movies) {
+                const omdbResponse = await fetch(`${this.proxyUrl}/api/details?id=${movie.imdbID}`)
+                const omdbData = await omdbResponse.json()
+                Object.assign(movie, omdbData)
+                
+                const rtResponse = await fetch(`${this.proxyUrl}/api/rottentomatoes?id=${movie.imdbID}`)
+                const rtData = await rtResponse.json()
+                movie.rtScore = rtData.score
+                movie.rtUrl = rtData.url
+                
+                const streamResponse = await fetch(`${this.proxyUrl}/api/streaming?id=${movie.imdbID}`)
+                const streamData = await streamResponse.json()
+                movie.streamingInfo = streamData
+            }
+        }, 
+        deduplicate(movies) {
+            const seen = new Set() 
+            return movies.filter(movie => {
+                if (seen.has(movie.imdbID)) return false
+                seen.add(movie.imdbID)
+                return true
+            })
         }
     }
 }).mount('#app')
