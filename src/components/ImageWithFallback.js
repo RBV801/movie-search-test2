55401983a61dee76287f7407a09570968bdae097
RBV801 <rakmunbv@gmail.com>
2024-12-04 11:16:53 -0800

    Simplify SVG placeholder to resolve vector file error

--- src/components/ImageWithFallback.js
+++ src/components/ImageWithFallback.js
@@ -2,68 +2,32 @@ import React, { useState } from 'react';
 
 const CyberpunkPlaceholder = () => (
   <svg
-    viewBox="0 0 300 450"
     xmlns="http://www.w3.org/2000/svg"
-    className="w-full h-full"
+    viewBox="0 0 300 450"
+    style={{ width: '100%', height: '100%' }}
   >
-    {/* Background Grid */}
-    <defs>
-      <pattern id="grid" width="30" height="30" patternUnits="userSpaceOnUse">
-        <path
-          d="M 30 0 L 0 0 0 30"
-          fill="none"
-          stroke="#ffffff10"
-          strokeWidth="1"
-        />
-      </pattern>
-      
-      {/* Glow Filter */}
-      <filter id="glow" x="-50%" y="-50%" width="200%" height="200%">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="2" result="blur" />
-        <feColorMatrix
-          in="blur"
-          mode="matrix"
-          values="
-            1 0 0 0 0
-            0 1 0 0 0
-            0 0 1 0 0
-            0 0 0 18 -7"
-          result="glow"
-        />
-        <feMerge>
-          <feMergeNode in="glow" />
-          <feMergeNode in="SourceGraphic" />
-        </feMerge>
-      </filter>
-    </defs>
-
-    {/* Background */}
     <rect width="100%" height="100%" fill="#121212" />
-    <rect width="100%" height="100%" fill="url(#grid)" />
-
-    {/* Skull Design */}
-    <g transform="translate(150, 225)" filter="url(#glow)">
-      <path
-        d="M-50 -60 C-50 -90, 50 -90, 50 -60 L50 20 C50 50, -50 50, -50 20 Z"
-        fill="none"
-        stroke="#00fff2"
-        strokeWidth="2"
-      />
-      {/* Eyes */}
-      <path
-        d="M-30 -20 L-10 -20 L-10 0 L-30 0 Z M10 -20 L30 -20 L30 0 L10 0 Z"
-        fill="#ff00ea"
-      />
-      {/* Teeth */}
-      <path
-        d="M-30 20 L30 20 L20 40 L-20 40 Z"
-        fill="none"
-        stroke="#00fff2"
-        strokeWidth="2"
-      />
-    </g>
-
-    {/* Text */}
+    <path
+      d="M0 0h300v450h-300z"
+      fill="none"
+      stroke="#ffffff10"
+      strokeWidth="1"
+      strokeDasharray="30"
+    />
+    <path
+      d="M100 165C100 135 200 135 200 165V245C200 275 100 275 100 245Z"
+      fill="none"
+      stroke="#00fff2"
+      strokeWidth="2"
+    />
+    <rect x="120" y="205" width="20" height="20" fill="#ff00ea" />
+    <rect x="160" y="205" width="20" height="20" fill="#ff00ea" />
+    <path
+      d="M120 245h60l-10 20h-40z"
+      fill="none"
+      stroke="#00fff2"
+      strokeWidth="2"
+    />
     <text
       x="150"
       y="350"
@@ -71,7 +35,6 @@ const CyberpunkPlaceholder = () => (
       fontSize="24"
       fill="#00fff2"
       textAnchor="middle"
-      filter="url(#glow)"
     >
       NO_SIGNAL//
     </text>
