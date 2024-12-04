e19ea5da37eb222141bddbb0e420189655a59a30
RBV801 <rakmunbv@gmail.com>
2024-12-04 11:18:58 -0800

    Switch to external SVG file for placeholder

--- src/components/ImageWithFallback.js
+++ src/components/ImageWithFallback.js
@@ -1,41 +1,12 @@
 import React, { useState } from 'react';
-
-const CyberpunkPlaceholder = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 300 450"
-    width="100%"
-    height="100%"
-    preserveAspectRatio="xMidYMid meet"
-  >
-    <rect width="300" height="450" fill="#121212" />
-    <text
-      x="150"
-      y="225"
-      fontSize="24"
-      fill="#00fff2"
-      textAnchor="middle"
-      dominantBaseline="middle"
-    >
-      NO IMAGE
-    </text>
-  </svg>
-);
+import placeholderImage from '../assets/placeholder.svg';
 
 const ImageWithFallback = ({ src, alt, className, ...props }) => {
   const [error, setError] = useState(false);
 
-  if (error) {
-    return (
-      <div style={{ width: '100%', height: '100%', position: 'relative' }}>
-        <CyberpunkPlaceholder />
-      </div>
-    );
-  }
-
   return (
     <img
-      src={src}
+      src={error ? placeholderImage : src}
       alt={alt}
       className={className}
       onError={() => setError(true)}
